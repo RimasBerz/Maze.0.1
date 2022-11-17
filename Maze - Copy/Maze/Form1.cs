518cbe327c78f6e95e7eccb00118916cf7829605
RimasBerz <116100732+RimasBerz@users.noreply.github.com>
2022-11-18 01:16:53 +0200

    Add files via upload

---
+++ Maze - Copy/Maze/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Input;
+namespace Maze
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Options();
+            StartGame();
+        }
+
+        public void Options()
+        {
+            Text = "Maze";
+
+            BackColor = Color.FromArgb(255, 92, 118, 137);
+
+            int sizeX = 40;
+            int sizeY = 20;
+
+            Width = sizeX * 16 + 16;
+            Height = sizeY * 16 + 40;
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        public void StartGame() {
+            Labirint l = new Labirint(this, 40, 20);
+            l.Show();
+        }
+
+        
+
+        private void Down(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void Up(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void Left(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void Right(object sender, System.EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
---
+++ Maze - Copy/Maze/Labirint.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Maze
+{
+    class Labirint
+    {
+        public int height;
+        public int width; 
+
+        public MazeObject[,] maze;
+        public PictureBox[,] images;
+
+        public static Random r = new Random();
+        public Form parent;
+
+        public Labirint(Form parent, int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            this.parent = parent;
+
+            maze = new MazeObject[height, width];
+            images = new PictureBox[height, width];
+
+            Generate();
+        }
+        int smileX = 0;
+        int smileY = 2;
+
+        private void Generate()
+        {
+            
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    MazeObject.MazeObjectType current = MazeObject.MazeObjectType.HALL;
+
+              
+                    if (r.Next(4) == 0)
+                    {
+                        current = MazeObject.MazeObjectType.WALL;
+                    }
+
+
+                    if (r.Next(250) == 0)
+                    {
+                        current = MazeObject.MazeObjectType.MEDAL;
+                    }
+
+                    
+                    if (r.Next(250) == 0)
+                    {
+                        current = MazeObject.MazeObjectType.ENEMY;
+                    }
+
+                   
+                    if (y == 0 || x == 0 || y == height - 1 | x == width - 1)
+                    {
+                        current = MazeObject.MazeObjectType.WALL;
+                    }
+
+                   
+                    if (x == smileX && y == smileY)
+                    {
+                        current = MazeObject.MazeObjectType.CHAR;
+                    }
+                    
+
+                    if (x == smileX + 1 && y == smileY || x == width - 1 && y == height - 3)
+                    {
+                        current = MazeObject.MazeObjectType.HALL;
+                    }
+                    maze[y, x] = new MazeObject(current);
+                    images[y, x] = new PictureBox();
+                    images[y, x].Location = new Point(x * maze[y, x].width, y * maze[y, x].height);
+                    images[y, x].Parent = parent;
+                    images[y, x].Width = maze[y, x].width;
+                    images[y, x].Height = maze[y, x].height;
+                    images[y, x].BackgroundImage = maze[y, x].texture;
+                    images[y, x].Visible = false;
+                }
+            }
+        }
+        private void Down_Click(object sender, System.EventArgs e)
+        {
+           //MazeObject.MazeObjectType position1 = MazeObject.MazeObjectType.CHAR;
+            
+            images[smileX, smileY].Location = new Point(smileX - 1,smileY);
+        }
+        private void Up_Click(object sender, System.EventArgs e)
+        {
+            //MazeObject.MazeObjectType position1 = MazeObject.MazeObjectType.CHAR;
+           
+            images[smileX, smileY].Location = new Point(smileX + 1, smileY);
+        }
+        private void Right_Click(object sender, System.EventArgs e)
+        {
+            //MazeObject.MazeObjectType position1 = MazeObject.MazeObjectType.CHAR;
+
+            images[smileX, smileY].Location = new Point(smileX, smileY + 1);
+        }
+        private void Left_Click(object sender, System.EventArgs e)
+        {
+            //MazeObject.MazeObjectType position1 = MazeObject.MazeObjectType.CHAR;
+
+            images[smileX, smileY].Location = new Point(smileX, smileY - 1);
+        }
+        public void Show()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    images[y, x].Visible = true;
+                    
+                }
+            }
+        }
+    }
+}
